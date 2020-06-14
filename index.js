2f972a0dd3edca5bb022aaab15a8e6054150f1e9
Choi YongKwon <puzzle75@naver.com>
2020-06-15 00:16:14 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://cyk:abcd1234@boilerplate-jxr2b.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World! ㅋㅋㅋ'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
